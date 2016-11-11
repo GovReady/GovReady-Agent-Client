27b50e5ed58ad4958ee8e4ecf492c72f243eeb04
Alex Schmoe <aschmoe@gmail.com>
2016-11-11 11:41:07 -0800

    making the routing login not load util initt'd

--- src/main.js
+++ src/main.js
@@ -8,31 +8,30 @@ import makeRoutes from './routes';
 import Root from './containers/Root';
 import configureStore from './redux/configureStore';
 
-// Configure history for react-router
-const browserHistory = useRouterHistory(createHashHistory)({
-  basename: __BASENAME__
-});
-
-// Create redux store and sync with react-router-redux. We have installed the
-// react-router-redux reducer under the key "router" in src/routes/index.js,
-// so we need to provide a custom `selectLocationState` to inform
-// react-router-redux of its location.
-const initialState = window.__INITIAL_STATE__;
-const store = configureStore(initialState, browserHistory);
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState: (state) => state.router
-});
-
-// Now that we have the Redux store, we can create our routes. We provide
-// the store to the route definitions so that routes have access to it for
-// hooks such as `onEnter`.
-const routes = makeRoutes(store);
-
 // Now that redux and react-router have been configured, we can render the
 // React application to the DOM!
 
 // Don't render if we're not logged in (do render if we have an application setting)
 if (window.govready == undefined || window.govready.application || !window.govready.connectUrl || window.govready.access_token) {
+  // Configure history for react-router
+  const browserHistory = useRouterHistory(createHashHistory)({
+    basename: __BASENAME__
+  });
+
+  // Create redux store and sync with react-router-redux. We have installed the
+  // react-router-redux reducer under the key "router" in src/routes/index.js,
+  // so we need to provide a custom `selectLocationState` to inform
+  // react-router-redux of its location.
+  const initialState = window.__INITIAL_STATE__;
+  const store = configureStore(initialState, browserHistory);
+  const history = syncHistoryWithStore(browserHistory, store, {
+    selectLocationState: (state) => state.router
+  });
+
+  // Now that we have the Redux store, we can create our routes. We provide
+  // the store to the route definitions so that routes have access to it for
+  // hooks such as `onEnter`.
+  const routes = makeRoutes(store);
   ReactDOM.render(
     <Root history={history} routes={routes} store={store} />,
     document.getElementById('root')
